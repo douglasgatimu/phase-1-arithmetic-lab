b35a19d3bd54c84d282de9693de8b1cd96a26301
Douglas Gatimu <douglasgatimu@gmail.com>
2025-03-19 08:20:27 +0300

    Completed arithmetic lab.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2
+let multiply = num1 * num2
+
+//console.log(multiply);
+let random = Math.floor(Math.random() * 10) + 1;
+
+//console.log(random);
+
+let num3 = 39;
+let num4 = 7;
+let mod = 39 % 7;
+
+let max = Math.max(2, 7, 4, 20);
